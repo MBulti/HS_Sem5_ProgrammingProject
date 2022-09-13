b0a1a9a48af3463022b7d9ec4fda3d35cd04cbb6
HardyJL <j.hardenberg@hotmail.de>
2022-09-13 13:31:29 +0200

    redo changes to main

--- main.py
+++ main.py
@@ -1,53 +1,38 @@
-from flask import Flask, request, jsonify
+"""
+Implementation of Programming Project Flask API
+This returns the movie recommendation based on a entered movie
+"""
+import os
+from flask import Flask, request
+from flask_restful import Api, Resource
+
 app = Flask(__name__)
 
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from the url parameter /getmsg/?name=
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"Received: {name}")
-
-    response = {}
-
-    # Check if the user sent a name at all
-    if not name:
-        response["ERROR"] = "No name found. Please send a name."
-    # Check if the user entered a number
-    elif str(name).isdigit():
-        response["ERROR"] = "The name can't be numeric. Please send a string."
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
-
-    # Return the response in json format
-    return jsonify(response)
+api = Api(app)
 
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome API!",
-            # Add this option to distinct the POST request
-            "METHOD": "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "No name found. Please send a name."
-        })
+class Recommendation(Resource):
+    """
+    Recommendation Endpoint, takes the watched movie as a string
+    """
+
+    def get(self):
+        args = request.args;
+        print(args)
+
+        """
+        Retuns the movie input
+        """
+        data={"data": args['movie']}
+
+        return data
+
+api.add_resource(Recommendation,'/recommendation')
 
 
-@app.route('/')
-def index():
-    # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+if __name__=='__main__':
+    cfg_port = os.getenv('PORT', "5000")
 
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(host="0.0.0.0", port=cfg_port, debug=True)
+#Test
