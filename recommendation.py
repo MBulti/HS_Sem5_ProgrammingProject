9007879006d944d26256d6d28c0ff093d091c5f8
HardyJL <33319906+HardyJL@users.noreply.github.com>
2022-09-14 21:42:52 +0200

    feature api calls for recommendation and movie list (#4)
    * feature api calls for recommendation and movie list
    basic implementation of data gathering

---
+++ recommendation.py
@@ -0,0 +1,26 @@
+import csv
+
+# os, multiprocessing as mp
+
+# from itertools import count
+# from multiprocessing import process
+
+listOfMovies = []
+listOfRecommendations = []
+
+
+def getListOfMovies():
+    global listOfMovies
+    with open("data/movie_titles.csv", newline='', encoding='latin-1') as f:
+        reader = csv.reader(f) 
+        if not listOfMovies:          
+            listOfMovies = list(reader)
+        return listOfMovies
+
+
+def getListOfRecommendations(movies):
+    return movies
+
+
+def getRecommendation():
+    return None
