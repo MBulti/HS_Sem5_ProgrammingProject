208752193d20f9aa54b7e7efa59ca14cb36bbb8c
HardyJL <j.hardenberg@hotmail.de>
2022-10-04 08:37:29 +0200

    fix movies feature recommendation in url refactoring

--- functions/data_output.py
+++ functions/data_output.py
@@ -2,11 +2,9 @@ import pandas as pd
 from models.movie import Movie
 from models.recommendation import Recommendation
 
-listOfMovies = []
-
 
 def getListOfMovies():
-    global listOfMovies
+    listOfMovies = []
     with open("data/movie_titles.csv", encoding='latin-1') as f:
         for line in f:
             listOfMovies.append(create_movie(line))
@@ -15,20 +13,24 @@ def getListOfMovies():
 
 def create_movie(line):
     movie = line.split(',')
-    movie_id = movie[0]
-    movie_title = movie[1]
-    movie = movie[2:-1]
-    if (movie.count('') < len(movie)):
+    movie[-1] = movie[-1].replace('\n', '')
+    movie_id = movie.pop(0)
+    movie_date = movie.pop(0)
+    if (len(movie) > 1):
+        movie_title = movie.pop(0)
         movie_title += ','.join(movie)
-    return Movie(id=movie_id, title=movie_title)
+    else:
+        movie_title = movie[0]
+    return Movie(id=int(movie_id), release_year=movie_date, title=movie_title)
 
 
 def getListOfRecommendations(movies: list[int]):
     movie_recommendations = []
     for movie in movies:
-        if(movie > 17700):
+        if (movie > 17700):
             raise ValueError('The given id is not an actual movie')
         df = pd.read_csv('data/recommendation.csv', header=None)
         recommendations = (df.loc[df[0] == movie].values).tolist()[0]
-        movie_recommendations.append(Recommendation(movie_id=movie, recommendations=recommendations[1:6]))
-    return(movie_recommendations)
+        movie_recommendations.append(Recommendation(
+            movie_id=movie, recommendations=recommendations[1:6]))
+    return movie_recommendations
